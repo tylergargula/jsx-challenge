ff7c23c02f62b278aec82e5d9d920757b7304f29
Tyler Gargula <64625949+tylergargula@users.noreply.github.com>
2021-06-02 22:26:48 -0500

    Second commit completed challenge

--- src/index.js
+++ src/index.js
@@ -2,3 +2,18 @@
 //It should display a h1 heading.
 //It should display an unordered list (bullet points).
 //It should contain 3 list elements.
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>My Favorite Foods</h1>
+    <ul>
+      <li>Chickpea Tikka Masala</li>
+      <li>Ramen</li>
+      <li>Pizza</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
